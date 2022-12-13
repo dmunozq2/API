9be6c31086394348ebbf95530ec38ec2aabe223d
dmunozq2 <119079525+dmunozq2@users.noreply.github.com>
2022-12-12 22:21:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import WebServices.Asynchtask;
+import WebServices.WebService;
+
+public class MainActivity extends AppCompatActivity implements Asynchtask {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws= new WebService("https://gorest.co.in/public/v1/users",
+                datos, MainActivity.this, MainActivity.this);
+        ws.execute("GET");
+    }
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+        TextView txtlista= findViewById(R.id.txtlista);
+        //txtlista.setText(result);
+        ArrayList<String> lstBancos = new ArrayList<String> ();
+        JSONObject JSONobjeto =  new JSONObject(result);
+        JSONArray JSONlista= JSONobjeto.getJSONArray("data");
+        String Lista="";
+        for(int i=0; i< JSONlista.length();i++){
+            JSONObject usuario=  JSONlista.getJSONObject(i);
+            Lista= Lista + usuario.getString("name") + "\n";
+        }
+        txtlista.setText(Lista);
+
+    }
+}
+
